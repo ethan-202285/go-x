5caca881d6143f4225a18dc79d1ece050c1b78f8
William Wong <goodwong@foxmail.com>
2019-08-31 22:41:48 +0800

    设置数据库open_id为not null;

--- auth/models.go
+++ auth/models.go
@@ -18,9 +18,9 @@ type User struct {
 // UserIdentity 登录方式(小程序、钉钉……密码都可以）
 type UserIdentity struct {
 	UserID   uint64
-	Provider string           `gorm:"primary_key"`
-	OpenID   string           `gorm:"primary_key"` // 第三方主键
-	Data     *json.RawMessage ``                   // json原始数据（密码也可以放这里）
+	Provider string           `gorm:"primary_key;not null"`
+	OpenID   string           `gorm:"primary_key;not null"` // 第三方主键
+	Data     *json.RawMessage ``                            // json原始数据（密码也可以放这里）
 }
 
 // UserLog 用户行为记录（登录、注销）
